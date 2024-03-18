5ce4c0c025076d372419496056b625e9f1b4cb51
Rayen Manaa <elmanaa.rayen@gmail.com>
2024-03-18 18:15:26 +0100

    change homepage

--- app/page.js
+++ app/page.js
@@ -2,16 +2,23 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Problem from "@/components/Problem";
 import Footer from "@/components/Footer";
+import Pricing from "@/components/Pricing";
+import FAQ from "@/components/FAQ";
+import CTA from "@/components/CTA";
 
 export default function Home() {
     return (
         <>
-            <Header/>
-            <main>
-                <Hero/>
-                <Problem/>
-            </main>
-            <Footer/>
+          <Header />
+          <main>
+            <Hero />
+            <Problem />
+            {/* <FeaturesAccordion /> */}
+            <Pricing />
+            <FAQ />
+            <CTA />
+          </main>
+          <Footer />
         </>
     );
 }
